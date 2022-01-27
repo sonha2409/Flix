105241ee90ddd5aefad9fbd5c6cf54216a4856df
Son Ha <sonhathai.2409@gmail.com>
2022-01-27 17:44:56 -0500

    Data exchange between screen

--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -60,5 +60,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.posterView.af.setImage(withURL: posterUrl!)
         return cell
             }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = TableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+//        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
 }
 
